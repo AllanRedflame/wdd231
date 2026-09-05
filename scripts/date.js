8898831be20d373e5ddc75f91f4bfa45a82e4cf9
AllanRedflame <Allanredflame@gmail.com>
2026-09-04 21:40:35 -0600

    testing colors

--- scripts/date.js
+++ scripts/date.js
@@ -3,5 +3,5 @@ const modified = document.lastModified;
 
 let copyright = document.getElementById("current-year");
 
-document.getElementById("last-updated").innerHTML = "Last updated: " + modified;
+document.getElementById("lastModified").innerHTML = "Last updated: " + modified;
 copyright.innerHTML = `David Poulsen | Utah | Copyright ${year}`;
