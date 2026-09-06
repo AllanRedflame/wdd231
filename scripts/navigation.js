5a835d8652dca2cef27adb8cbffac79a539a749e
AllanRedflame <Allanredflame@gmail.com>
2026-09-05 20:47:55 -0600

    Try this one

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,14 +1,3 @@
-const box = document.getElementById("nav-box");
-
-  box.addEventListener("mouseover", () => {
-    box.style.backgroundColor = "lightblue";
-  });
-
-  box.addEventListener("mouseout", () => {
-    box.style.backgroundColor = "white";
-  });
-
-
 const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("nav-box");
 
