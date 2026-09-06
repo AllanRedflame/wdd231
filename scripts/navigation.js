f40a9454ebd71644f28f1ddf111d58a506a62e10
AllanRedflame <Allanredflame@gmail.com>
2026-09-05 18:26:47 -0600

    Trying this js

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -7,3 +7,12 @@ const box = document.getElementById("nav-box");
   box.addEventListener("mouseout", () => {
     box.style.backgroundColor = "white";
   });
+
+
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("nav-box");
+
+hamburger.addEventListener('click', () => {
+  menu.classList.toggle('show');
+  hamburger.classList.toggle('open'); // optional for animation
+});
