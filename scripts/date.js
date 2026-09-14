737251871054eee3fc3331ed5fe8d4f2c6e1479a
AllanRedflame <Allanredflame@gmail.com>
2026-09-14 15:11:27 -0600

    Many updates

--- scripts/date.js
+++ scripts/date.js
@@ -4,5 +4,5 @@ const modified = document.lastModified;
 let copyright = document.getElementById("current-year");
 
 document.getElementById("lastModified").innerHTML = "Last updated: " + modified;
-copyright.innerHTML = `David Poulsen | Utah | Copyright ${year}`;
+copyright.append(` ${year} Murray Chamber of Commerce`);
 
