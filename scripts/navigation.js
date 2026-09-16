edf4ad33bd7ed9407dffbe2ae797bc11bc7bc0b3
AllanRedflame <Allanredflame@gmail.com>
2026-09-15 19:44:01 -0600

    done with small view? That was fast.

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,5 +1,5 @@
 const hamburger = document.getElementById("hamburger");
-const menu = document.getElementById("nav-box");
+const menu = document.getElementById("nav-dropdown");
 
 hamburger.addEventListener('click', () => {
   menu.classList.toggle('show');
